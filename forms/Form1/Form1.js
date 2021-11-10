34728dbea5ba47f068c19509ad69e821a1c1a027
Vinny Cindy Student <80773478+cindyccStudent@users.noreply.github.com>
2021-11-09 19:33:54 -0600

    Update Form1.js

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -2,12 +2,6 @@
 Button1.onclick=function(){
 
   Label1.value = "Hello this is the main program talking."
-<<<<<<< Updated upstream
-=======
 
-=======
-  Label1.value = "Hello this is Cindy Student talking frm her branch."
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 }
 
